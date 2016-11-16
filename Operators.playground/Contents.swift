94902d8bc3efa2b50dd0d40388c10be33e8171cc
VinicioSantos <vinicio.csantos@outlook.com>
2016-11-16 11:02:03 -0200

    Exercício de Operações
    Apenas um treino de operadores

---
+++ Operators.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var message = "Hello, playground"
+
+var x = 100
+
+var y = 30
+
+var z = x + y
+
+var verdade = x == y ? "Igual" : "diferente"
+
+var verdade2 = x == 100 ? "verdade" : "mentira"
