a9ae4c682aab9c50995675853ba922ce85df8044
VinicioSantos <vinicio.csantos@outlook.com>
2016-11-28 13:41:42 -0200

    Loops

---
+++ Loops.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var count = 0
+
+repeat{
+print(count)
+    count += 1
+    
+}while(count <= 10)
+
+
+var salarios = [1000,2200,2250,2700,3100,3500]
+//seria como um for otimizado do Java
+for salario in salarios{
+    print(salario)
+}
